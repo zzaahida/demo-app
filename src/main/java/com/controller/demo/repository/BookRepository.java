06e5e8dc35a7d65a44142d4628efcf52e9b84f20
Zahida <zahida.yermagambet@mail.ru>
2022-05-07 23:56:39 +0600

    lab10

--- src/main/java/com/controller/demo/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.controller.demo.repository;
-
-import com.controller.demo.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/resources/templates/Query.sql
+++
@@ -1,8 +0,0 @@
-delete from books;
-
-select * from users;
-
-select * from books;
-
-insert into books(id, author, category, title, year, owner_id)
-values(1, 'Author - 1', 'Category - 1', 'Title - 1', 2002, 8)
